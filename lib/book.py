35f44f0c17167ef5b27115fcf7458e7151b302ed
CALEB KOMEN <komencaleb502@gmail.com>
2024-05-27 12:50:09 +0300

    jhd

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
+    def __init__(self, title, author, pages):
+        self._title = title
+        self._author = author
+        self.pages = pages
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        self._author = author
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages):
+        if not isinstance(pages, int) or pages <= 0:
+            raise ValueError("Pages must be a positive integer")
+        self._pages = pages
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size, material):
+        self._brand = brand
+        self.size = size
+        self._material = material
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, brand):
+        self._brand = brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, (int, float)) or size <= 0:
+            raise ValueError("Size must be a positive number")
+        self._size = size
+
+    @property
+    def material(self):
+        return self._material
+
+    @material.setter
+    def material(self, material):
+        self._material = material
